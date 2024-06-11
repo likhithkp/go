de5c1058fe51e85064c9a8fac00e7ca7889eaca3
likhithkp <likhithkpdev.com>
2024-06-11 08:25:26 +0530

    Practie

--- main.go
+++ main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+type Car struct {
+	Name  string
+	Fuel  string
+	Model int
+}
+
+func createCar(car Car) Car {
+	car.Name = car.Name
+	car.Fuel = car.Fuel
+	car.Model = car.Model
+
+	return car
+}
+
 func main() {
-	fmt.Println("Hello")
+	newCar := createCar(Car{
+		Name:  "BMW",
+		Fuel:  "Diesel",
+		Model: 2022,
+	})
+
+	fmt.Println(newCar)
 }
