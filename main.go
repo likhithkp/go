e143a3ad2191a93d1b74057e685fc57136401314
likhithkp <likhithkpdev@gmail.com>
2024-06-20 06:48:08 +0530

    Go

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import "fmt"
-
-type car struct {
-	brand string
-	fuel  string
-	year  int
-	model string
-}
-
 func main() {
-	newCar := car{
-		brand: "BMW",
-		fuel:  "Diesel",
-		year:  2023,
-		model: "X5",
-	}
 
-	fmt.Println(newCar.brand, newCar.fuel)
 }
